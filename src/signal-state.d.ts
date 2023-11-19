7e2ce0f35920b7d005c463af069305d98e61cbac
GitHub <noreply@github.com>
2023-11-19 03:06:19 +0000

    213e4c92 fix(data): DefaultDataService getAll httpOptions fix + test (#4134)

--- src/signal-state.d.ts
+++ src/signal-state.d.ts
@@ -1,9 +1,9 @@
 import { WritableSignal } from '@angular/core';
 import { DeepSignal } from './deep-signal';
 export declare const STATE_SIGNAL: unique symbol;
-export type SignalStateMeta<State extends Record<string, unknown>> = {
+export type SignalStateMeta<State extends object> = {
     [STATE_SIGNAL]: WritableSignal<State>;
 };
-type SignalState<State extends Record<string, unknown>> = DeepSignal<State> & SignalStateMeta<State>;
-export declare function signalState<State extends Record<string, unknown>>(initialState: State): SignalState<State>;
+type SignalState<State extends object> = DeepSignal<State> & SignalStateMeta<State>;
+export declare function signalState<State extends object>(initialState: State): SignalState<State>;
 export {};

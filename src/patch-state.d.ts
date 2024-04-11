8b55375318e6e418eeb3d6219abe6e8c31f9c6ea
GitHub <noreply@github.com>
2024-04-11 08:09:42 +0000

    6b440ee3 fix(signals): make patchState work with TS 5.4 (#4294)

--- src/patch-state.d.ts
+++ src/patch-state.d.ts
@@ -1,3 +1,4 @@
 import { StateSignal } from './state-signal';
+import { Prettify } from './ts-helpers';
 export type PartialStateUpdater<State extends object> = (state: State) => Partial<State>;
-export declare function patchState<State extends object>(stateSignal: StateSignal<State>, ...updaters: Array<Partial<State & {}> | PartialStateUpdater<State & {}>>): void;
+export declare function patchState<State extends object>(stateSignal: StateSignal<State>, ...updaters: Array<Partial<Prettify<State>> | PartialStateUpdater<Prettify<State>>>): void;

15021f1ec756623d3953e1246d41337e01a1911e
GitHub <noreply@github.com>
2024-07-22 13:37:01 +0000

    3e15fcb8 feat(signals): remove RxMethodInput for better readability (#4445)

--- rxjs-interop/src/rx-method.d.ts
+++ rxjs-interop/src/rx-method.d.ts
@@ -1,7 +1,6 @@
 import { Injector, Signal } from '@angular/core';
 import { Observable, Unsubscribable } from 'rxjs';
-type RxMethodInput<Input> = Input | Observable<Input> | Signal<Input>;
-type RxMethod<Input> = ((input: RxMethodInput<Input>) => Unsubscribable) & Unsubscribable;
+type RxMethod<Input> = ((input: Input | Signal<Input> | Observable<Input>) => Unsubscribable) & Unsubscribable;
 export declare function rxMethod<Input>(generator: (source$: Observable<Input>) => Observable<unknown>, config?: {
     injector?: Injector;
 }): RxMethod<Input>;

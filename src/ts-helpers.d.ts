7a2dcaa86fa5074549c8fbaa44fde269893cf2a7
GitHub <noreply@github.com>
2024-07-23 17:53:42 +0000

    1858a511 feat(signals): allow defining private SignalStore members (#4447)

--- src/ts-helpers.d.ts
+++ src/ts-helpers.d.ts
@@ -4,3 +4,6 @@ export type Prettify<T> = {
 export type IsRecord<T> = T extends object ? T extends unknown[] ? false : T extends Set<unknown> ? false : T extends Map<unknown, unknown> ? false : T extends Function ? false : true : false;
 export type IsUnknownRecord<T> = string extends keyof T ? true : number extends keyof T ? true : false;
 export type IsKnownRecord<T> = IsRecord<T> extends true ? IsUnknownRecord<T> extends true ? false : true : false;
+export type OmitPrivate<T> = {
+    [K in keyof T as K extends `_${string}` ? never : K]: T[K];
+};
